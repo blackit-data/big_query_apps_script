41d6885e1252e89553c02ab9eaba83a8f15f366d
huschdie <huschdie@users.noreply.github.com>
2017-06-20 13:17:16 +0300

    Update write_Table.js

--- scripts/write_Table.js
+++ scripts/write_Table.js
@@ -1,16 +1,25 @@
-function write_Table(sql,projectId,datasetId,tableId,writeDisposition) {
+function write_Table(sql,projectId,datasetId,tableId,writeDisposition,legacy_sql) {
   
-  //  writeDisposition in (WRITE_TRUNCATE, WRITE_APPEND,WRITE_EMPTY)
+  // writeDisposition in (WRITE_TRUNCATE, WRITE_APPEND,WRITE_EMPTY)
   // default : WRITE_EMPTY
   
    if(typeof writeDisposition == "undefined"){
     writeDisposition = 'WRITE_EMPTY'
   }
+  
+   if(typeof legacy_sql == "undefined"){ 
+       var legacy_sql = true
+         } else if(legacy_sql==0 || legacy_sql == false || legacy_sql == 'standard'){ 
+            var legacy_sql = false
+            } else {
+              var legacy_sql = true
+              }
     
   var job = {
     configuration: {
       query: {
         query: sql,
+        useLegacySql:legacy_sql,
         writeDisposition:writeDisposition,
         destinationTable: {
           projectId: projectId,
